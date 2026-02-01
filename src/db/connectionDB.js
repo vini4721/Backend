14ff8a9b1c5ac7b0bae4298ea9e7e31d943cd049
Vinayak Singh <vinayaksingh836@gmail.com>
2026-02-02 02:04:02 +0530

    established database connection

---
+++ src/db/connectionDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log("Connection established");
+  } catch (error) {
+    console.log("connection error", error);
+  }
+};
+
+export default connectDB;
